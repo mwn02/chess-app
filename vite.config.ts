4f29f187ad5460f2d261da354c4f4b37caa5068e
mwn <mwn.boullette@gmail.com>
2023-04-16 18:34:14 -0400

    add repo to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/chess-app/", // change to your repo name
   plugins: [svelte()],
-})
+});
